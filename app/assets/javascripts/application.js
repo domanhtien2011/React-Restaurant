4c1519d13850fe9fd4a0374d176cbd3cc9603e56
Tien Do <domanhtien2011@gmail.com>
2016-04-05 13:18:45 +0700

    flux pattern

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,14 @@
+//= require jquery
+//= require jquery_ujs
+//= require foundation
+//= require lodash
+//= require turbolinks
+//= require react
+//= require react_ujs
+//= require flux
+//= require eventemitter
+//= require components
+//= require app
+//= require_tree .
+
+$(function(){ $(document).foundation(); });
